5d01de4ea8ffc5cb817e2743b6f0a06eb384ef4a
xiaoke <xiaokexkxk@126.com>
2020-09-27 23:59:37 +0800

    BubbleSort

---
+++ src/com/xk/algorithms/sorts/BubbleSort.java
@@ -0,0 +1,39 @@
+package com.xk.algorithms.sorts;
+
+import java.util.Arrays;
+
+/**
+ * @Author Ke Xiao <xiaokexkxk@126.com>
+ * @Date 2020/9/27 23:02
+ * @Version 1.0
+ */
+public class BubbleSort {
+
+    public static void main(String[] args) {
+        Integer[] arr = {3, 5, 4, 6, 7, 8, 3, 4, 5, 6, 2, 1, 10};
+        BubbleSort.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static <T extends Comparable<T>> T[] sort(T[] unsorted) {
+        int length = unsorted.length;
+        boolean swapFlag = true;
+        while (swapFlag) {
+            swapFlag = false;
+            for (int i = 1; i < length; i++) {
+                if (unsorted[i].compareTo(unsorted[i - 1]) < 0) {
+                    swap(unsorted, i, i - 1);
+                    swapFlag = true;
+                }
+            }
+            length--;
+        }
+        return unsorted;
+    }
+
+    public static <T extends Comparable<T>> void swap(T[] unsorted, int index1, int index2) {
+        T temp = unsorted[index1];
+        unsorted[index1] = unsorted[index2];
+        unsorted[index2] = temp;
+    }
+}
